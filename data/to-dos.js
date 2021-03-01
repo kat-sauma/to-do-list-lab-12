4cddcd32f4164e555d6210084bbc3688d4fae680
Kat Sauma <katsauma@gmail.com>
2021-03-01 15:32:43 -0800

    created tables, drop tables, load seed data, updated app.js endpoints and setup database. tested through postman

---
+++ data/to-dos.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    todo: 'feed chi chi',
+    completed: false,
+    importance: 'high',
+    user_id: 1
+  },
+  {
+    todo: 'walk chi chi',
+    completed: false,
+    importance: 'high',
+    user_id: 1
+  },
+  {
+    todo: 'fold laundry',
+    completed: false,
+    importance: 'low',
+    user_id: 1
+  },
+  {
+    todo: 'call your mom (and dad)',
+    completed: false,
+    importance: 'medium',
+    user_id: 1
+  },
+  {
+    todo: 'water the plants',
+    completed: false,
+    importance: 'low',
+    user_id: 1
+  },
+  {
+    todo: 'order groceries',
+    completed: false,
+    importance: 'high',
+    user_id: 1
+  },
+  {
+    todo: 'visit Lauren, James, and bebe Julian',
+    completed: false,
+    importance: 'medium',
+    user_id: 1
+  }
+];
